a9ab43937c6c84000c918ebb5d15de724a57a4b7
YevaMV <eva280988@gmail.com>
2022-01-05 23:06:33 +0400

    html, css changes, added computer choice

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const startGameBtn = document.getElementById('start-game-btn'); 
+const takePickbtn = document.getElementById('take-pick-btn'); 
+const startGamebtn = document.getElementById('start-game-btn');
 
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
@@ -14,13 +15,29 @@ const getPlayerChoice = function() {
   ) {
     document.getElementById('user-choice').innerHTML = `Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`;
     return DEFAULT_USER_CHOICE;
- 
   }
   document.getElementById('user-choice').innerHTML = `Your choice is ${selection}!!!`;
-  return selection;
- 
+//   return selection;
 };
 
-startGameBtn.addEventListener('click', function() {
+const computerChoice = function() {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        document.getElementById('computer-choice').innerHTML = `Player's choice is ${ROCK}!!!`;
+        // return ROCK;
+    } else if (randomValue < 0.67) {
+        document.getElementById('computer-choice').innerHTML = `Player's choice is ${PAPER}!!!`;
+        // return PAPER;
+    } else {
+        document.getElementById('computer-choice').innerHTML = `Player's choice is ${SCISSORS}!!!`;
+        // return SCISSORS;
+    }
+}
+
+takePickbtn.addEventListener('click', function() {
   const playerSelection = getPlayerChoice();
 });
+
+startGamebtn.addEventListener('click', function() {
+    const computerSelection = computerChoice();
+})
