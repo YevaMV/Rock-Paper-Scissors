1aaf1498b43bff5c108f751b184cf4edd2c76663
YevaMV <eva280988@gmail.com>
2022-01-05 13:30:21 +0400

    adding user choices

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById('start-game-btn'); 
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = function() {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+  if (
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
+  ) {
+    document.getElementById('user-choice').innerHTML = `Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`;
+    return DEFAULT_USER_CHOICE;
+ 
+  }
+  document.getElementById('user-choice').innerHTML = `Your choice is ${selection}!!!`;
+  return selection;
+ 
+};
+
+startGameBtn.addEventListener('click', function() {
+  const playerSelection = getPlayerChoice();
+});
